6a47099c447c721581e7f67460138a431aff67ec
Zikai-Zhang <zikai9@ualberta.ca>
2026-03-04 20:16:15 -0700

    added test CountCities

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -24,4 +24,19 @@ public class CustomListTest {
         assertDoesNotThrow(() -> cityList.delete(calgary));
         assertThrows(IllegalArgumentException.class, () -> cityList.delete(calgary));
     }
+
+    @Test
+    void testCountCities() {
+        CustomList cityList = new CustomList();
+
+        assertEquals(0, cityList.countCities());
+
+        City calgary = new City("Calgary", "AB");
+        cityList.addCity(calgary);
+        assertEquals(1, cityList.countCities());
+
+        City halifax = new City("Halifax", "Nova Scotia");
+        cityList.addCity(halifax);
+        assertEquals(2, cityList.countCities());
+    }
 }
